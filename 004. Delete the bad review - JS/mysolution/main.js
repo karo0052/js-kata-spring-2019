1dfbcfc764f3d94bfa3caffe5fc35a8d5ad22c66
Karoline Lüthje <Karoline@Karolines-MacBook-Pro-4.local>
2019-03-06 20:52:01 +0100

    kata 004

---
+++ 004. Delete the bad review - JS/mysolution/main.js
@@ -0,0 +1,21 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", init);
+
+function init() {
+  console.log("init");
+
+  loop("span", 5);
+}
+
+function loop(element, rating) {
+  console.log("loop");
+
+  document.querySelectorAll(element).forEach(review => {
+    const score = review.innerHTML;
+
+    if (score <= rating) {
+      review.parentNode.remove();
+    }
+  });
+}
